46048123575b1d52d1762e86da0248bb0146cdd1
Nada-AlMahdy <nadaalmahdy@std.mans.edu.eg>
2025-04-19 19:55:03 +0200

    test
    try

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num=300;
+    char ch;
+    ch=num;
+    int i1=3 , i2=2;
+    float f,f1;
+    f1=i1;
+    f=(float)i1/i2;
+    printf("%f\n",f);
+    printf("%d\n",ch);
+    return 0;
+}
